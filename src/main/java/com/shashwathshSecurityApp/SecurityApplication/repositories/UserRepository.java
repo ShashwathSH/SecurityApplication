4cd1231d03fc3a37333677213ede14df27297dad
ShashwathSH <shashwathshashu15@gmail.com>
2026-01-14 17:21:01 +0530

    Security filter chain

---
+++ src/main/java/com/shashwathshSecurityApp/SecurityApplication/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.shashwathshSecurityApp.SecurityApplication.repositories;
+
+import com.shashwathshSecurityApp.SecurityApplication.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    @Query("select u from User u where u.email = ?1")
+    User findByEmail(String email);
+
+
+}
